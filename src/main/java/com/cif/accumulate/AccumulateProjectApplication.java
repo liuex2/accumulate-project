21faefb3603d5237a1da63259f2dea510dd89564
liuex2 <1853199517@qq.com>
2019-05-24 18:31:51 +0800

    commit base file

---
+++ src/main/java/com/cif/accumulate/AccumulateProjectApplication.java
@@ -0,0 +1,16 @@
+package com.cif.accumulate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: liuxincai
+ * @Description: A application about the project
+ * @Date: 2019/5/24 18:24
+ */
+@SpringBootApplication
+public class AccumulateProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccumulateProjectApplication.class, args);
+    }
+}
