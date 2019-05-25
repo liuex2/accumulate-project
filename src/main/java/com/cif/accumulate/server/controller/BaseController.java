6573ce4b9fe4c7da423d6e2250583195e528d021
liuex2 <1853199517@qq.com>
2019-05-25 17:48:38 +0800

    commit base file

---
+++ src/main/java/com/cif/accumulate/server/controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.cif.accumulate.server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: liuxincai
+ * @Description: 基础控制器
+ * @Date: 2019/5/25 11:49
+ */
+@RestController
+public class BaseController {
+}
