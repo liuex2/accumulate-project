3ec84e815b432f4c4a1ae1457e6d85daf2d8bf88
liuex2 <1853199517@qq.com>
2019-06-28 17:06:06 +0800

    commit test file

---
+++ src/test/com/cif/accumulate/Java8Test.java
@@ -0,0 +1,56 @@
+package com.cif.accumulate;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: liuxincai
+ * @Description: java8新特性
+ * @Date: 2019/6/24 11:05
+ */
+public class Java8Test {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+
+    @Test
+    public void lamadaTest(){
+        String[] args = {"23434","4343","948320"};
+
+        //转换成集合
+        List<String> list = Arrays.asList(args);
+
+        //lamada表达式
+        list.forEach(arg->{
+            System.out.println(arg);
+        });
+
+        //::应用
+        list.forEach(
+                System.out::println
+        );
+
+    }
+
+    @Test
+    public void streamTest(){
+        String[] args = {"23434","23434","4343","4343","948320"};
+
+        //转换集合
+        List<String> list = Arrays.asList(args);
+
+        //利用stream去重
+        List<String> collect = list.stream().distinct().collect(Collectors.toList());
+
+        collect.forEach(
+                System.out::println
+        );
+
+    }
+
+}
